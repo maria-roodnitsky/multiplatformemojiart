24f0ba1d5c5dc6aa3ad64681895ec51ffc8f756b
Maria Roodnitsky <maria.roodnitsky@gmail.com>
2022-05-11 17:16:14 -0700

    Multiplatform

---
+++ Shared/EmojiArtApp.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Maria Roodnitsky on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    @StateObject var paletteStore = PaletteStore(named: "Default")
+    
+    var body: some Scene {
+        DocumentGroup(newDocument: { EmojiArtDocument() }) { config in
+            EmojiArtDocumentView(document: config.document)
+                .environmentObject(paletteStore)
+        }
+    }
+}
